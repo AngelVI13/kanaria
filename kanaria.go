d474e50dea1361a3523c55d899998eb31e6dbefe
Angel ILIEV <a.v.iliev13@gmail.com>
2019-08-22 08:52:27 +0300

    Initial commit

--- kanaria.go
+++ kanaria.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("knight.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(50, 50)
+	//op.GeoM.Scale(1.5, 1)
+	screen.DrawImage(img, op)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello")
+	if err := ebiten.Run(update, 640, 480, 1, "Hello World"); err != nil {
+		log.Fatal(err)
+	}
 }
